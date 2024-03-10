5a90066d7f19131f32d00bf69bba5b4707726a91
abhijithu1 <abhiunni9656@gmail.com>
2024-03-10 21:20:58 +0530

    issue 1 fixed: abhiunni9656@gmail.com , i'm not able to load the website (i just installed npm).

--- my-app/src/helpers/calculateWinner.js
+++ my-app/src/helpers/calculateWinner.js
@@ -1,22 +1,21 @@
 function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 0],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8], // diagonal top-left to bottom-right
+    [2, 4, 6], // diagonal top-right to bottom-left
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
-    //you might need this 'classicpolo'
-    return null;
   }
+  return null;
+}
 
   export default calculateWinner;
